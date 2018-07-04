c3cb8a93771ba2668922ba4fb93d3352a32855d2
Jeffeson Chaves <jefferson.chaves@ifc.edu.br>
2018-07-04 09:23:26 -0300

    atualizando projeto consultaCep

--- ConsultaCEP/app/src/main/java/br/edu/ifc/araquari/aula/consultacep/ConsultaActivity.java
+++ ConsultaCEP/app/src/main/java/br/edu/ifc/araquari/aula/consultacep/ConsultaActivity.java
@@ -2,9 +2,17 @@ package br.edu.ifc.araquari.aula.consultacep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.ExecutionException;
+
+import model.CEP;
+import service.CorreiosService;
 
 public class ConsultaActivity extends AppCompatActivity {
 
@@ -25,5 +33,17 @@ public class ConsultaActivity extends AppCompatActivity {
 
         String cep = edtCEP.getText().toString();
 
+        try {
+
+            CEP cepRetorno = new CorreiosService(cep).execute().get();
+            tvResultado.setText(cepRetorno.toString());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(ConsultaActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- ConsultaCEP/app/src/main/java/service/CorreiosService.java
+++ ConsultaCEP/app/src/main/java/service/CorreiosService.java
@@ -1,6 +1,15 @@
 package service;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 import model.CEP;
 
@@ -10,10 +19,58 @@ public class CorreiosService extends AsyncTask<Void, Void, CEP>{
 
     public CorreiosService(String cep){
 
+        Log.d("AST", "construtor");
+
+        if (cep != null && cep.length() == 8){
+            this.cep = cep;
+        } else {
+            throw new IllegalArgumentException("CEP invalido");
+        }
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Log.i("AST", "onPreExecute");
+
+    }
+
+    @Override
+    protected void onPostExecute(CEP cep) {
+        super.onPostExecute(cep);
+        Log.i("AST", "onPostExecute");
+
     }
 
     @Override
     protected CEP doInBackground(Void... voids) {
-        return null;
+        Log.i("AST", "doInBackground");
+
+        StringBuilder resposta = new StringBuilder();
+
+        try{
+            URL url = new URL("https://viacep.com.br/ws/"+this.cep+"/json/");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.connect();
+
+            Scanner scanner = new Scanner(url.openStream());
+
+            while (scanner.hasNext()){
+                resposta.append(scanner.next());
+            }
+
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return new Gson().fromJson(resposta.toString(), CEP.class);
     }
 }
